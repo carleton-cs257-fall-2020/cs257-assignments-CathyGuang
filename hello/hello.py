4727e1c3f57c1e010600efd9477787a2f4feb90d
wenlaihan <69778334+wenlaihan@users.noreply.github.com>
2020-09-16 21:39:58 -0500

    last version

--- hello/hello.py
+++ hello/hello.py
@@ -1,9 +1,5 @@
 ##########################################This is a HelloWorld File!
 print("Hello World")
-<<<<<<< HEAD
 print("Hello CS")
-print("hi")
-=======
-<<<<<<< HEAD
 print("CS 257 is great!")
->>>>>>> e930f51a9aea3b4ea6df494a25e4ac37cc97fb31
+print("Thanks for your hard work grading our assignment!")

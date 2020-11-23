5112cec257b37ed660b74d1941ce22753bbe201b
Ziyun Guang <Cathy@eduroam-176-104.dyn.carleton.edu>
2020-11-23 10:07:26 -0600

    updated

--- webapp/pages/static/topdriver.js
+++ webapp/pages/static/topdriver.js
@@ -9,31 +9,45 @@ function getAPIBaseURL() {
     return baseURL;
 }
 
-function createDriverBarChart() {
-    var url = getAPIBaseURL() + '/topdriver/20';
+function createDriverBarChart(topNumber) {
+    var url = getAPIBaseURL() + '/topdriver/' + topNumber;
 
     fetch(url, { method: 'get' })
 
     .then((response) => response.json())
 
     .then(function(top_driver_list) {
+        var labelList = [];
+        var dataList = [];
+        for (var k = 0; k < top_driver_list.length; k++) {
+            var top_driver = top_driver_list[k];
+            labelList.push(top_driver[0]);
+            dataList.push(top_driver[1]);
 
-        // var label_list = [];
-        // for (var k = 0; k < top_driver_list.length; k++) {
-        //     var top_driver = top_driver_list[k];
-        //     label_list.push(top_driver);
-        // }
-        var data = {
-            labels: [Mon, Tue],
-            series: [
-                [17, -2]
-            ]
-        };
-
-        var options = {
-            seriesBarDistance: 10
         }
-        new Chartist.Bar('#sample-bar-chart', data, options);
+        var data = {
+
+            labels: labelList,
+            series: [dataList]
+        };
+        if (topNumber == 50) {
+            var options = {
+                seriesBarDistance: 10,
+                horizontalBars: true,
+                reverseData: true
+            }
+        } else {
+            var options = {
+                axisX: {
+                    position: 'start'
+                },
+                axisY: {
+                    position: 'end'
+                }
+            };
+        }
+
+        new Chartist.Bar('#topdriver-bar-chart', data, options);
     })
 
     .catch(function(error) {
--- webapp/pages/static/webapp.js
+++ webapp/pages/static/webapp.js
@@ -20,15 +20,22 @@ function load20thCenRaceTable() {
 
     .then(function(list_of_20_races) {
         var tableBody = '';
+        tableBody += '<tr>';
+        tableBody += '<td>' + 'Race Name' + '</td>';
+        tableBody += '<td>' + 'Date' + '</td>';
+        tableBody += '<td>' + 'Country' + '</td>';
+        tableBody += '<td>' + 'Location' + '</td>';
+        tableBody += '<td>' + 'Circuit Name' + '</td>';
+        tableBody += '</tr>';
         for (var k = 0; k < list_of_20_races.length; k++) {
             var race = list_of_20_races[k];
             tableBody += '<tr>';
-            tableBody += '<td>' + race['Name'] + '</td>';
+            // tableBody += '<td>' + race['Name'] + '</td>';
+            tableBody += '<td>' + '<a href=' + race['URL'] + '>' + race['Name'] + '</a>' + '</td>';
             tableBody += '<td>' + race['Date'] + '</td>';
             tableBody += '<td>' + race['Country'] + '</td>';
             tableBody += '<td>' + race['Location'] + '</td>';
             tableBody += '<td>' + race['Circuit'] + '</td>';
-            tableBody += '<td>' + race['URL'] + '</td>';
             tableBody += '</tr>';
         }
         var race20ListElement = document.getElementById('20centuryRace');
@@ -51,15 +58,23 @@ function load21thCenRaceTable() {
 
     .then(function(list_of_21_races) {
         var tableBody = '';
+        tableBody += '<tr>';
+        tableBody += '<td>' + 'Race Name' + '</td>';
+        tableBody += '<td>' + 'Date' + '</td>';
+        tableBody += '<td>' + 'Country' + '</td>';
+        tableBody += '<td>' + 'Location' + '</td>';
+        tableBody += '<td>' + 'Circuit Name' + '</td>';
+        tableBody += '</tr>';
         for (var k = 0; k < list_of_21_races.length; k++) {
             var race = list_of_21_races[k];
             tableBody += '<tr>';
-            tableBody += '<td>' + race['Name'] + '</td>';
+            // tableBody += '<td>' + race['Name'] + '</td>';
+            tableBody += '<td>' + '<a href=' + race['URL'] + '>' + race['Name'] + '</a>' + '</td>';
             tableBody += '<td>' + race['Date'] + '</td>';
             tableBody += '<td>' + race['Country'] + '</td>';
             tableBody += '<td>' + race['Location'] + '</td>';
             tableBody += '<td>' + race['Circuit'] + '</td>';
-            tableBody += '<td>' + race['URL'] + '</td>';
+            // tableBody += '<td>' + race['URL'] + '</td>';
             tableBody += '</tr>';
         }
         var race21ListElement = document.getElementById('21centuryRace');
@@ -89,7 +104,12 @@ function initializeMap() {
 }
 
 function doneMap(dataMap) {
-    dataMap.svg.selectAll('.datamaps-subunit').on('click', clickCountryDriver, clickCountryRace);
+    dataMap.svg.selectAll('.datamaps-subunit').on('click', onClickCountry);
+}
+
+function onClickCountry(geography) {
+    clickCountryRace(geography);
+    clickCountryDriver(geography);
 }
 
 function clickCountryRace(geography) {
@@ -101,17 +121,30 @@ function clickCountryRace(geography) {
 
     .then(function(list_of_country_races) {
         var tableBody = '';
-        for (var k = 0; k < list_of_country_races.length; k++) {
-            var race = list_of_country_races[k];
+        if (list_of_country_races.length == 0) {
             tableBody += '<tr>';
-            tableBody += '<td>' + '<a href=' + race['URL'] + '>' + race['Name'] + '</a>' + '</td>';
-            tableBody += '<td>' + race['Date'] + '</td>';
-            tableBody += '<td>' + race['Country'] + '</td>';
-            tableBody += '<td>' + race['Location'] + '</td>';
-            tableBody += '<td>' + race['Circuit'] + '</td>';
-            //tableBody += '<td>' + race['URL'] + '</td>';
+            tableBody += '<td>' + 'No Race Available for This Country' + '</td>';
             tableBody += '</tr>';
+        } else {
+            tableBody += '<tr>';
+            tableBody += '<th>' + 'Race Name' + '</th>';
+            tableBody += '<th>' + 'Date' + '</th>';
+            tableBody += '<th>' + 'Country' + '</th>';
+            tableBody += '<th>' + 'Location' + '</th>';
+            tableBody += '<th>' + 'Circuit Name' + '</th>';
+            tableBody += '</tr>';
+            for (var k = 0; k < list_of_country_races.length; k++) {
+                var race = list_of_country_races[k];
+                tableBody += '<tr>';
+                tableBody += '<td>' + '<a href=' + race['URL'] + '>' + race['Name'] + '</a>' + '</td>';
+                tableBody += '<td>' + race['Date'] + '</td>';
+                tableBody += '<td>' + race['Country'] + '</td>';
+                tableBody += '<td>' + race['Location'] + '</td>';
+                tableBody += '<td>' + race['Circuit'] + '</td>';
+                tableBody += '</tr>';
+            }
         }
+
         var countryRaceSummaryElement = document.getElementById('country_race');
         if (countryRaceSummaryElement) {
             countryRaceSummaryElement.innerHTML = tableBody;
@@ -132,14 +165,24 @@ function clickCountryDriver(geography) {
 
     .then(function(list_of_country_drivers) {
         var tableBody = '';
-        for (var k = 0; k < list_of_country_drivers.length; k++) {
-            var driver = list_of_country_drivers[k];
+        if (list_of_country_drivers.length == 0) {
             tableBody += '<tr>';
-            tableBody += '<td>' + '<a href=' + driver['URL'] + '>' + driver['Forename'] + driver['Surname'] + '</a>' + '</td>';
-            tableBody += '<td>' + driver['Date of Birth'] + '</td>';
-            tableBody += '<td>' + driver['Nationality'] + '</td>';
-            // tableBody += '<td>' + driver['URL'] + '</td>';
+            tableBody += '<td>' + 'No Driver Available for This Country' + '</td>';
             tableBody += '</tr>';
+        } else {
+            tableBody += '<tr>';
+            tableBody += '<th>' + 'Driver Name' + '</th>';
+            tableBody += '<th>' + 'Date of Birth' + '</th>';
+            tableBody += '<th>' + 'Nationality' + '</th>';
+            tableBody += '</tr>';
+            for (var k = 0; k < list_of_country_drivers.length; k++) {
+                var driver = list_of_country_drivers[k];
+                tableBody += '<tr>';
+                tableBody += '<td>' + '<a href=' + driver['URL'] + '>' + driver['Forename'] + driver['Surname'] + '</a>' + '</td>';
+                tableBody += '<td>' + driver['Date of Birth'] + '</td>';
+                tableBody += '<td>' + driver['Nationality'] + '</td>';
+                tableBody += '</tr>';
+            }
         }
         var countryDriverSummaryElement = document.getElementById('country_driver');
         if (countryDriverSummaryElement) {
